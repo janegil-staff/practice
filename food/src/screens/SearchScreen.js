a019b45b00f68a82b5ff5b0eafd4e667d1a9e9e0
Jan Egil Staff Øvretveit <janstove@gmail.com>
2023-12-23 10:24:28 +0100

    FOOD: Connect to Yelp api

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
-
+import axios from "axios";
 const SearchScreen = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const options = {
+    method: "GET",
+    url: "https://api.yelp.com/v3/businesses/search",
+    params: { location: "Bergen", sort_by: "best_match", limit: "20" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer VHdZASGXTZTBOMCmUGe7mH4T8xcn-GLrr4hn5eLG6PAjUopHeOJ_KUPJUoyctg7JHzSj4P9IVjtmLhbVHnOY_dPAmAyazjDoJIryqeSP7A6mkrGeRy0n7z5eLZWGZXYx",
+    },
+  };
+  const searchHandler = () => {
+    axios
+      .request(options)
+      .then(function (response) {
+        setResults(response.data.businesses);
+      })
+      .catch((error) => {
+        setErrorMessage("Something went wrong");
+      });
+  };
   return (
     <View>
-      <SearchBar term={term} 
-      onTermChange={newTerm => setTerm(newTerm)} 
-      onTermSubmit={() => console.log('Term was submitted!')} />
-      <Text>{term}</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={searchHandler}
+      />
+      <Text>{errorMessage}</Text>
+      <Text>We have found {results.length}</Text>
     </View>
   );
 };
